727af1cf447e06251e5d7aca09379c15e667316f
A-Macleod <0308180@uhi.ac.uk>
2025-03-31 21:12:13 +0100

    AlarmForm2 / AlarmPresenter - Added data validation to the AddAlarm method called in AlarmView
    Validates the name, hours and minutes.
    If there is an error, call ShowError() function in the AlarmView

--- ClockV2/Presenter/AlarmPresenter.cs
+++ ClockV2/Presenter/AlarmPresenter.cs
@@ -1,25 +1,65 @@
-﻿using ClockV2.Model;
+﻿using ClockV2.Interface;
+using ClockV2.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ClockV2
 {
 
     public class AlarmPresenter
     {
-        private readonly AlarmView view;
-        private readonly AlarmModel model; 
+        private readonly AlarmView _view;
+        private readonly AlarmModel _model; 
 
-        public AlarmPresenter(AlarmView view, AlarmModel model)
+        public AlarmPresenter(AlarmView _view, AlarmModel _model)
         {
-            this.view = view;
-            this.model = model;
+            this._view = _view;
+            this._model = _model;
             
             // Link the View to this Presenter
-            view.SetPresenter(this);
+            _view.SetPresenter(this);
         }
+
+
+        public void AddAlarm(string alarmName,string priorityHour,string priorityMinute)
+        {
+
+            try
+            {
+
+                if (string.IsNullOrWhiteSpace(alarmName))
+                {
+
+                    _view.ShowError("Not Valid");
+                }
+
+
+                if (!int.TryParse(priorityHour, out _)){    // discard the output
+
+                    _view.ShowError("Not a number");
+                }
+
+
+                if(!int.TryParse(priorityMinute, out _))    // discard the output
+                {
+                    _view.ShowError("Not a number");
+                }
+
+                _model.AddAlarm(alarmName,priorityHour,priorityMinute);
+
+            }
+            catch(Exception ex)
+            {
+                _view.ShowError(ex.Message); // Call the ShowError Popup function in the UI View
+            }
+
+        }
+
+
+
     }
 }
