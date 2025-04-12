718b5430d97222e710d6b2bbafbf5338ba337f3a
A-Macleod <0308180@uhi.ac.uk>
2025-04-12 16:24:36 +0100

    Alarm / Modified the Alarm Class [NOT FINISHED]

--- ClockV2/Alarm.cs
+++ ClockV2/Alarm.cs
@@ -17,32 +17,72 @@ namespace ClockV2
     /// </summary>
     public class Alarm
     {
+
+        private int _initialAlarmTime { get; }
+        private DateTime _currentDateTime;
+        
         public string AlarmName { get; set; }
         public int AlarmTime { get; set; }
-        private int _initialAlarmTime { get; }
+
         public System.Windows.Forms.Timer Timer;
 
-        //private SortedArrayPriorityQueue<Alarm> _alarms;   /////////// FOR TESTING ACCESSING ALARMMODEL FUNCTIONS
 
+        public EventHandler Alarm_Countdown_Reached_Zero;
+        public EventHandler Alarm_Time_Remove_Alarm;
+
+
+        //private SortedArrayPriorityQueue<Alarm> _alarms;   /////////// FOR TESTING ACCESSING ALARMMODEL FUNCTIONS
+        // https://stackoverflow.com/questions/30008530/timespan-countdown-timer
+        // https://geoffstratton.com/cnet-countdown-timer/
+        // https://zerotomastery.io/blog/c-sharp-timespan/ TIMESPAN EXPLANATION 
 
         /// <summary>
         /// Constructor, takes in name and time and creates the alarm object with a Timer, that has a 1 second interval.
         /// At each interval the AlarmTimer_Tick eventhandler is called, which handles the alarm countdown. 
         /// </summary>
         /// <param name="alarmName">The name of the alarm</param>
-        /// <param name="alarmTime">The total time of the alarm</param>
-        public Alarm(string alarmName, int alarmTime)
+        /// <param name="alarmCountdownTimeInSeconds">The total time of the alarm countdown in seconds</param>
+        public Alarm(string alarmName, int alarmCountdownTimeInSeconds)
         {
 
             AlarmName = alarmName;
-            AlarmTime = alarmTime;
+            AlarmTime = alarmCountdownTimeInSeconds;
+
             _initialAlarmTime = AlarmTime;
+
             Timer = new System.Windows.Forms.Timer();
             Timer.Interval = 1000;
+
             Timer.Tick += Timer_Tick_Countdown;
             
         }
 
+
+
+        /// <summary>
+        /// Eventhandler that CountsDown with each TimerTick. The AlarmTime is decremented and when
+        /// it reaches zero a message box will be called displaying that the alarm has now sounded.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Timer_Tick_Countdown(object sender, EventArgs e)
+        {
+
+            _currentDateTime = DateTime.Now;
+
+
+            AlarmTime--;
+
+
+
+            if (AlarmTime <= 0)
+            {
+                StopCountdownAndDispose();
+                MessageBox.Show($"Alarm : {AlarmName}\r\nis Ready!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                //ResetAlarm();             
+            }
+        }
 
         /// <summary>
         /// Method to Start the Timer of the Alarm object
@@ -61,7 +101,7 @@ namespace ClockV2
         public void StopCountdownAndDispose()
         {
             Timer.Stop();
-            Timer.Tick -= Timer_Tick_Countdown; // Unsubscribe from TickEvent
+            Timer.Tick -= Timer_Tick_Countdown; // Unsubscribe from TickEvent, prevents memory leaks
             Timer.Dispose();
         }
 
@@ -73,26 +113,7 @@ namespace ClockV2
         }
 
 
-        /// <summary>
-        /// Eventhandler that CountsDown with each TimerTick. The AlarmTime is decremented and when
-        /// it reaches zero a message box will be called displaying that the alarm has now sounded.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Timer_Tick_Countdown(object sender, EventArgs e)
-        {
-            AlarmTime--;
 
-            
-            if (AlarmTime <= 0)
-            {
-                StopCountdownAndDispose();
-                MessageBox.Show($"Alarm : {AlarmName}\r\nis Ready!", "Alarm", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                //ResetAlarm();             
-            }
-
-        }
 
 
         //public TimeSpan GetRemainingTime()
